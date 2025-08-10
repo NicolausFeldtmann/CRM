76aaf5e0401a2e2d7db3861526317821fc0550fd
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-08-10 17:26:01 +0200

    add user button &dialog added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatDialog } from '@angular/material/dialog'
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIcon,
+    MatTooltipModule,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  constructor(private dialog: MatDialog) { }
+
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
